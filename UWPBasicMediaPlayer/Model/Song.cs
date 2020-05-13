ed711ca8f9796f289022d3df138fb5688da25c4e
Ana GOMEZ-MAYEN <31400695+anagomezmayen@users.noreply.github.com>
2020-05-12 23:27:20 -0700

    new Classes:Song

---
+++ UWPBasicMediaPlayer/Model/Song.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace UWPBasicMediaPlayer.Model
+{
+    /** Media element could be music or video*/
+    public class Song
+    {
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public string Title { get; set; }
+        public bool IsFavorite { get; set; }
+        public string SongFile { get; set; }
+
+        public Song(string pathToFile)
+        {
+            TagLib.File tagFile = TagLib.File.Create(pathToFile);
+            Artist = (string)tagFile.Tag.FirstAlbumArtist;
+            Album = (string)tagFile.Tag.Album;
+            Title = (string)tagFile.Tag.Title;
+            SongFile = pathToFile;
+            IsFavorite = false; //meanwhile this property has a default value
+
+        }
+    }
+}
---
+++ UWPBasicMediaPlayer/Model/SongManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPBasicMediaPlayer.Model
+{
+    public static class SongManager
+    {
+        public static void GetAllSongs(System.Collections.ObjectModel.ObservableCollection<Song> songs)
+        {
+            var allSongs = GetSongs();
+            songs.Clear();
+            allSongs.ForEach(song => songs.Add(song));
+
+        }
+        /**
+         * This method read the name files in the carpet music and make the list of Songs
+         */
+        public static List<Song> GetSongs()
+        {
+            var songs = new List<Song>();
+            string[] filePaths = Directory.GetFiles($"/Assets/music");
+            foreach (var filepath in filePaths)
+            {
+                songs.Add(new Song(filepath));
+            }
+            return songs;
+        }
+
+    }
+}
